649d24d01fee9414f4c4b29ebf0ffc6252c97479
USER <nkduy.07@gmail.com>
2026-01-17 00:12:08 +0700

    Assignment 2

---
+++ Assignment 2/Exercise 1.py
@@ -0,0 +1,6 @@
+size = float(input("Enter the size of the zander: "))
+if size >= 42:
+    print("Approved!")
+else:
+    print("Not approved! Please release the fish bro")
+    print("The fish is",42-size,"cm below the limit")
